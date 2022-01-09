8ee60975ca6a675803d1fcc6224b894ca5dc6aba
ks-mani <kumar.mani017@gmail.com>
2022-01-09 14:42:01 +0530

    Component1 - Part 2

--- src/Component1.js
+++ src/Component1.js
@@ -3,17 +3,47 @@ import React from 'react';
 class Component1 extends React.Component {
     constructor() {
         super();
-        console.log("%c[Component1.js]", 'color: green; font-size: 13px',"\tConstructor Called")
+        this.state = {
+            value: 'mani'
+        }
+        this.inputChangeHandler = this.inputChangeHandler.bind(this);
+        console.log("%c[Component1.js]", 'color: green; font-size: 13px',"\tConstructor Called");
     }
 
     componentDidMount() {
         console.log("%c[Component1.js]", 'color: green; font-size: 13px',"\tcomponentDidMount Called")
     }
 
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log("%c[Component1.js]", 'color: green; font-size: 13px',"\tgetSnapshotBeforeUpdate Called")
+        console.log("%c[Component1.js]", 'color: green; font-size: 13px',"\tPrev Props", prevProps)
+        console.log("%c[Component1.js]", 'color: green; font-size: 13px',"\tPrev State", prevState)
+
+        return "delhi"
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("%c[Component1.js]", 'color: green; font-size: 13px',"\tcomponentDidUpdate Called")
+        console.log("%c[Component1.js]", 'color: green; font-size: 13px',"\tPrev Props", prevProps)
+        console.log("%c[Component1.js]", 'color: green; font-size: 13px',"\tPrev State", prevState)
+        console.log("%c[Component1.js]", 'color: green; font-size: 13px',"\tSnapshot", snapshot)
+    }
+
+    inputChangeHandler(e) {
+        let val = e.target.value;
+        this.setState({
+            value: val
+        })
+    }   
+
     render() {
         console.log("%c[Component1.js]", 'color: green; font-size: 13px',"\tRender Called")
         return (
-            <p>Kumar</p>
+            <>
+                <div style={{marginTop: '20px'}}>
+                    <input type="text" name="name" value={this.state.value} onChange={this.inputChangeHandler}/>    
+                </div>
+            </>
         )
     }
 }
