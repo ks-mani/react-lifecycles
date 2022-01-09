d7a53633af127539c02eddaa0f0bc0ce4b8d0e4c
ks-mani <kumar.mani017@gmail.com>
2022-01-09 13:59:42 +0530

    Modifications in  App JS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import React from 'react';
 
-function App() {
-  return (
-    <p>mani</p>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showComponent: false
+    }
+    this.showComponentHandler = this.showComponentHandler.bind(this);
+  }
+
+  showComponentHandler(e) {
+    this.setState((state, props)=>{
+      console.log("%c[App.js]", 'color: red; font-size: 15px'," - Current State", state);
+      console.log("%c[App.js]", 'color: red; font-size: 15px'," - Current Props", props);
+      return {
+        showComponent: !state.showComponent
+      }
+    }, ()=>{
+      console.log("%c[App.js]", 'color: red; font-size: 15px'," - Set State Call Back")
+    })
+  } 
+
+  render() {
+    return (
+      <> 
+        <button onClick={this.showComponentHandler} >Show Component 1</button>
+        {
+          this.state.showComponent ? <p>Mani</p> : ''
+        }
+      </>
+    )
+  }
 }
 
 export default App;
